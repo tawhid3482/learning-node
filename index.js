cc756e12fc9f2473adbfeece1a6bbd8593b3284e
saikat <tawhidulislam3482@gmail.com>
2024-05-13 19:31:52 +0600

    path html

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const myModule = require('./local-1')
-console.log(myModule.add(2,5))
+// local module
+// const {a, add,  } = require("./local-1");
+// console.log(add(2, 5));
+// console.log(a)
+
+
+// build in module
+const path = require('path')
+console.log(path.dirname('/C:/Project-Level2/learning-node/index.js'))
+console.log(path.parse('/C:/Project-Level2/learning-node/index.js'))
+console.log(path.join('/C:/Project-Level2/learning-node/','local-1.js'))
