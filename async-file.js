a679f162dfc08ae99163623ddab1a734cd66a333
saikat <tawhidulislam3482@gmail.com>
2024-05-13 20:06:54 +0600

    fs read2 file async

--- async-file.js
+++ async-file.js
@@ -3,4 +3,7 @@ const fs = require('fs')
  fs.readFile('./texts/read.txt','utf-8', (error,data)=>{
     if(error) throw error
     console.log(data)
+    fs.writeFile('./texts/read2.txt',data,'utf-8',(error)=>{
+        if(error) throw error
+    })
  })
